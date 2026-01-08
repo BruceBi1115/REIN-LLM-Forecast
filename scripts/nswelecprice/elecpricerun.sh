03e579c3ac23930c998a394714358677c5dacf40
Bruce Bi <brucebi1115@hotmail.com>
2026-01-08 22:46:36 +1100

    close dropout when testing

--- scripts/nswelecprice/elecpricerun.sh
+++ scripts/nswelecprice/elecpricerun.sh
@@ -56,55 +56,55 @@ python run.py \
     --select_policy_by "epoch" \
     --template_pool configs/norl_template.yaml
     
-python run.py \
-    --taskName '[NoNews]NSW2015To2016ElectricityPriceWithNews' \
-    --time_col 'SETTLEMENTDATE'\
-    --value_col 'RRP'\
-    --unit "$/MWh" \
-    --description "This dataset records the electricity price data in Australia NSW from 2015 to 2016, collected from National electricity market." \
-    --region "Australia, NSW"\
-    --dayFirst True \
-    --train_file dataset/2015-2016NSWelecprice/2015To2016NSWData_trainset.csv \
-    --val_file dataset/2015-2016NSWelecprice/2015To2016NSWData_valset.csv \
-    --test_file dataset/2015-2016NSWelecprice/2015To2016NSWData_testset.csv \
-    --news_text_col 'summary_response' \
-    --news_time_col 'date' \
-    --keyword_path 'keywords/kws.txt' \
-    --epochs 30\
-    --keyword_number 20 \
-    --news_window_days 1 \
-    --news_topM 999 \
-    --news_topK 10 \
-    --batch_size 1\
-    --rl_use 1 \
-    --rl_algo "lints" \
-    --reward_metric "mse" \
-    --rl_cycle_steps 1 \
-    --select_policy_by "epoch"
+# python run.py \
+#     --taskName '[NoNews]NSW2015To2016ElectricityPriceWithNews' \
+#     --time_col 'SETTLEMENTDATE'\
+#     --value_col 'RRP'\
+#     --unit "$/MWh" \
+#     --description "This dataset records the electricity price data in Australia NSW from 2015 to 2016, collected from National electricity market." \
+#     --region "Australia, NSW"\
+#     --dayFirst True \
+#     --train_file dataset/2015-2016NSWelecprice/2015To2016NSWData_trainset.csv \
+#     --val_file dataset/2015-2016NSWelecprice/2015To2016NSWData_valset.csv \
+#     --test_file dataset/2015-2016NSWelecprice/2015To2016NSWData_testset.csv \
+#     --news_text_col 'summary_response' \
+#     --news_time_col 'date' \
+#     --keyword_path 'keywords/kws.txt' \
+#     --epochs 30\
+#     --keyword_number 20 \
+#     --news_window_days 1 \
+#     --news_topM 999 \
+#     --news_topK 10 \
+#     --batch_size 1\
+#     --rl_use 1 \
+#     --rl_algo "lints" \
+#     --reward_metric "mse" \
+#     --rl_cycle_steps 1 \
+#     --select_policy_by "epoch"
 
-python run.py \
-    --taskName 'NSW2015To2016ElectricityPriceWithNews' \
-    --time_col 'SETTLEMENTDATE'\
-    --value_col 'RRP'\
-    --unit "$/MWh" \
-    --description "This dataset records the electricity price data in Australia NSW from 2015 to 2016, collected from National electricity market." \
-    --region "Australia, NSW"\
-    --news_path dataset/Summarized_news_2015_2020.json \
-    --dayFirst True \
-    --train_file dataset/2015-2016NSWelecprice/2015To2016NSWData_trainset.csv \
-    --val_file dataset/2015-2016NSWelecprice/2015To2016NSWData_valset.csv \
-    --test_file dataset/2015-2016NSWelecprice/2015To2016NSWData_testset.csv \
-    --news_text_col 'summary_response' \
-    --news_time_col 'date' \
-    --keyword_path 'keywords/kws.txt' \
-    --epochs 30\
-    --keyword_number 20 \
-    --news_window_days 1 \
-    --news_topM 999 \
-    --news_topK 10 \
-    --batch_size 1\
-    --rl_use 1 \
-    --rl_algo "lints" \
-    --reward_metric "mse" \
-    --rl_cycle_steps 1 \
-    --select_policy_by "epoch"
+# python run.py \
+#     --taskName 'NSW2015To2016ElectricityPriceWithNews' \
+#     --time_col 'SETTLEMENTDATE'\
+#     --value_col 'RRP'\
+#     --unit "$/MWh" \
+#     --description "This dataset records the electricity price data in Australia NSW from 2015 to 2016, collected from National electricity market." \
+#     --region "Australia, NSW"\
+#     --news_path dataset/Summarized_news_2015_2020.json \
+#     --dayFirst True \
+#     --train_file dataset/2015-2016NSWelecprice/2015To2016NSWData_trainset.csv \
+#     --val_file dataset/2015-2016NSWelecprice/2015To2016NSWData_valset.csv \
+#     --test_file dataset/2015-2016NSWelecprice/2015To2016NSWData_testset.csv \
+#     --news_text_col 'summary_response' \
+#     --news_time_col 'date' \
+#     --keyword_path 'keywords/kws.txt' \
+#     --epochs 30\
+#     --keyword_number 20 \
+#     --news_window_days 1 \
+#     --news_topM 999 \
+#     --news_topK 10 \
+#     --batch_size 1\
+#     --rl_use 1 \
+#     --rl_algo "lints" \
+#     --reward_metric "mse" \
+#     --rl_cycle_steps 1 \
+#     --select_policy_by "epoch"
